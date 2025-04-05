9cb3d1da678fd1940d2e8669609ef5d6f7d19243
nublius <184036331+nublius@users.noreply.github.com>
2025-04-04 19:52:00 -0700

    Operate function

--- scripts.js
+++ scripts.js
@@ -13,3 +13,9 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let firstNum, operator, secondNum;
+
+function operate(operator, firstNum, secondNum) {
+   return operator(firstNum, secondNum);
+}
