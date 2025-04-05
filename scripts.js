0603802589e3b6ed6cbfe1f057f0bba3ec3519ab
nublius <184036331+nublius@users.noreply.github.com>
2025-04-05 12:46:57 -0700

    adding basic functionality and an updating display

--- scripts.js
+++ scripts.js
@@ -1,25 +1,71 @@
-function add(a, b) {
-    return a + b;
-}
+const display = document.querySelector(".display-text");
 
-function subtract(a, b) {
-    return a - b;
-}
+const buttons = document.querySelectorAll(".btn");
 
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
-const calculator = {
-    firstNum: null;
-    operator: null;
-    secondNum: null;
+const numbers = {
+    one: "1",
+    two: "2",
+    three: "3",
+    four: "4",
+    five: "5",
+    six: "6",
+    seven: "7",
+    eight: "8",
+    nine: "9",
+    zero: "0",
 };
 
-function operate(operator, firstNum, secondNum) {
-   return operator(firstNum, secondNum);
+const operations = {
+    add: "+",
+    subtract: "−",
+    multiply: "×",
+    divide: "÷",
 }
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        // CHECK for input
+        if (button.id in numbers) {
+            // IF firstNum exists, concatenate
+            if (calculator.firstNum === null) {
+                calculator.firstNum = numbers[button.id];
+                display.textContent = calculator.firstNum;
+            } else {
+                calculator.firstNum += numbers[button.id];
+                display.textContent = calculator.firstNum;
+            }
+        } else if (button.id in operations) {
+            calculator.operator = button.id;
+            console.log(calculator);
+            display.textContent = calculator.firstNum + " " + operations[button.id];
+        }
+    })
+});
+
+const calculator = {
+    firstNum: null,
+    operator: null,
+    secondNum: null,
+
+    add: function(a, b) {
+        return a + b;
+    },
+
+    subtract: function(a, b) {
+        return a - b;
+    },
+
+    multiply: function(a, b) {
+        return a * b;
+    },
+
+    divide: function(a, b) {
+        return a / b;
+    },
+
+    calculate: function(operator, firstNum, secondNum) {
+        return operator(firstNum, secondNum);
+    },
+
+    
+};
